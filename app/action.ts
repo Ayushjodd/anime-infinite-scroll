4d8adcc45820484fb31b38a555f2848bedf48468
Ayushjodd <ayushkalonia2016@gmail.com>
2024-07-25 22:47:41 +0530

    Finished the project

---
+++ app/action.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export const fetchAnime = async (page: number) => {
+  const res = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+  const data = res.json();
+  console.log(data);
+
+  return data;
+};
